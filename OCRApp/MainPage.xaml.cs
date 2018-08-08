fae71d56945d4b9f2e7b379def3c5a92ec224fe4
lincvic <lincvic@yahoo.com>
2018-08-08 16:49:32 +0800

    Update
    Update

---
+++ OCRApp/MainPage.xaml.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Graphics.Imaging;
+using Windows.Media.Capture;
+using Windows.Media.MediaProperties;
+using Windows.Storage;
+using Windows.Storage.FileProperties;
+using Windows.Storage.Pickers;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
+
+namespace OCRApp
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        MediaCapture mediaCapture = new MediaCapture();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+           //拍照
+            var myPictures = await Windows.Storage.StorageLibrary.GetLibraryAsync(Windows.Storage.KnownLibraryId.Pictures);
+            StorageFile file = await myPictures.SaveFolder.CreateFileAsync("photo.jpg", CreationCollisionOption.GenerateUniqueName);
+
+            using (var captureStream = new InMemoryRandomAccessStream())
+            {
+                await mediaCapture.CapturePhotoToStreamAsync(ImageEncodingProperties.CreateJpeg(), captureStream);
+
+                using (var fileStream = await file.OpenAsync(FileAccessMode.ReadWrite))
+                {
+                    var decoder = await BitmapDecoder.CreateAsync(captureStream);
+                    var encoder = await BitmapEncoder.CreateForTranscodingAsync(fileStream, decoder);
+
+                    var properties = new BitmapPropertySet {
+            { "System.Photo.Orientation", new BitmapTypedValue(PhotoOrientation.Normal, PropertyType.UInt16) }
+        };
+                    await encoder.BitmapProperties.SetPropertiesAsync(properties);
+
+                    await encoder.FlushAsync();
+                }
+
+
+            }
+
+          //选择照片
+            FileOpenPicker openPicker = new FileOpenPicker();
+            openPicker.ViewMode = PickerViewMode.Thumbnail;
+            openPicker.SuggestedStartLocation = PickerLocationId.PicturesLibrary;
+            openPicker.FileTypeFilter.Add(".jpg");
+            openPicker.FileTypeFilter.Add(".PNG");
+            StorageFile image = await openPicker.PickSingleFileAsync();
+            
+
+            BitmapImage bi = new BitmapImage();
+            if (image != null)
+            {
+                IRandomAccessStream ir = await image.OpenAsync(FileAccessMode.Read);
+                await bi.SetSourceAsync(ir);
+                imageControl.Source = bi;
+
+                
+            }
+
+
+          //请求API
+            var OCRresult = await OCRAPI.OCRRequests(image);
+
+            StringBuilder sb = new StringBuilder();
+
+            //遍历API结果
+            foreach (var region in OCRresult.regions)
+            {
+                foreach(var line in region.lines)
+                {
+                    foreach(var word in line.words)
+                    {
+                        sb.Append(word.text);
+                    }
+                }
+            }
+
+            resultText.Text = sb.ToString();
+ 
+
+        }
+
+        private async void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+           //显示预览
+            await mediaCapture.InitializeAsync();
+
+
+            myCaptureElement.Source = mediaCapture;
+            await mediaCapture.StartPreviewAsync();
+        }
+
+        
+       
+    }
+}
---
+++ OCRApp/OCRAPI.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.Storage.Streams;
+
+namespace OCRApp
+{
+
+    public class OCRAPI
+    {
+        // TODO SUB KEY
+        const string Key = "d994912f1378493092799049c6bdd228";
+
+        // TODO 请求地址
+        const string uriBase =
+            "https://eastasia.api.cognitive.microsoft.com/vision/v2.0/ocr";
+
+        
+        public static async Task<RootObject> OCRRequests(StorageFile image)
+        {
+
+            //请求
+            HttpClient client = new HttpClient();
+
+         
+            client.DefaultRequestHeaders.Add(
+                "Ocp-Apim-Subscription-Key", Key);
+
+        
+            string requestParameters = "language=zh-Hans&detectOrientation=true";
+
+           
+            string uri = uriBase + "?" + requestParameters;
+
+            HttpResponseMessage response;
+
+           //转Byte[]
+            byte[] byteData = await GetBytesAsync(image);
+
+            using (ByteArrayContent content = new ByteArrayContent(byteData))
+            {
+              
+                content.Headers.ContentType =
+                    new MediaTypeHeaderValue("application/octet-stream");
+
+               
+                response = await client.PostAsync(uri, content);
+            }
+
+        
+            string contentString = await response.Content.ReadAsStringAsync();
+
+            //JSON序列化
+            var result = await response.Content.ReadAsStringAsync();
+            var serializer = new DataContractJsonSerializer(typeof(RootObject));
+            var ms = new MemoryStream(Encoding.UTF8.GetBytes(result));
+
+            return (RootObject)serializer.ReadObject(ms);
+
+
+
+            
+        }
+
+      //StrogeFile 转 Byte[]
+        public static async Task<byte[]> GetBytesAsync(StorageFile file)
+        {
+            byte[] fileBytes = null;
+            if (file == null) return null;
+            using (var stream = await file.OpenReadAsync())
+            {
+                fileBytes = new byte[stream.Size];
+                using (var reader = new DataReader(stream))
+                {
+                    await reader.LoadAsync((uint)stream.Size);
+                    reader.ReadBytes(fileBytes);
+                }
+            }
+            return fileBytes;
+        }
+
+    }
+
+ 
+    //序列化封装
+    [DataContract]
+    public class Word
+    {
+        [DataMember]
+        public string boundingBox { get; set; }
+
+        [DataMember]
+        public string text { get; set; }
+    }
+
+    [DataContract]
+    public class Line
+    {
+        [DataMember]
+        public string boundingBox { get; set; }
+
+        [DataMember]
+        public List<Word> words { get; set; }
+    }
+
+    [DataContract]
+    public class Region
+    {
+        [DataMember]
+        public string boundingBox { get; set; }
+
+        [DataMember]
+        public List<Line> lines { get; set; }
+    }
+
+    [DataContract]
+    public class RootObject
+    {
+        [DataMember]
+        public string language { get; set; }
+
+        [DataMember]
+        public string orientation { get; set; }
+
+        [DataMember]
+        public double textAngle { get; set; }
+
+        [DataMember]
+        public List<Region> regions { get; set; }
+    }
+}
